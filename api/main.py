8315b1543ad999f29324bb899e9cfa0b89fa0ef2
Fabian Tenzara <fabiantenzara@gmail.com>
2025-07-09 10:38:29 +0700

    update code and config

--- api/main.py
+++ api/main.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.wsgi import WSGIMiddleware
-from .graph import app_dash, router as graph_router
+from .graph import app_dash
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
-app.include_router(graph_router, prefix="/graph", tags=["graph"])
 
 app.mount('/dash-layout', WSGIMiddleware(app_dash.server))
